2e6656a5a21e52df8aa5602d1fad102f7a9359e9
shigemk2 <i.am.shige@gmail.com>
2014-04-06 00:19:29 +0900

    Fix form

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,13 +1,9 @@
-package model
+package models
 
 case class Task(id: Long, label: String)
 
 object Task {
-  object Task {
-    def all(): List[Task] = Nil
-
-    def create(label: String) {}
-
-    def delete(id: Long) {}
-  }
+  def all(): List[Task] = Nil
+  def create(label: String) {}
+  def delete(id: Long) {}
 }
