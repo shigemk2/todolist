3cf4921c19bba9fe1dfe4aa9aec832764af15ccc
shigemk2 <i.am.shige@gmail.com>
2014-04-05 22:35:00 +0900

    Add config for routes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,13 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("Hello world")
   }
+
+  def tasks = TODO
+
+  def newTask = TODO
+
+  def deleteTask(id: Long) = TODO
 
 }
